4ef886c88a8e06a67d208b3857e1bdd4a625f898
bonot9434 <bonot9434@gmail.com>
2021-11-21 01:34:33 +0900

    アイテム追加機能

--- src/App.js
+++ src/App.js
@@ -11,16 +11,53 @@ import {
 
 const App = () => {
 	const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const handleAddButtonClick = () => {
+    const newItem = {
+      itemName: inputValue,
+      quantity: 1,
+      isSelected: false,
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setInputValue("");
+  };
 
 	return (
 		<div className="app-background">
 			<div className="main-container">
 				<div className="add-item-box">
-					<input className="add-item-input" placeholder="Add an item..." />
-					<FontAwesomeIcon icon={faPlus} />
+					<input value={inputValue} onChange={(event) => 
+          setInputValue(event.target.value)}className="add-item-input" placeholder="Add an item..." />
+					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
 				</div>
 				<div className="item-list">
-					<div className="item-container"></div>
+          {items.map((item,index) => (
+					  <div className="item-container">
+              <div className="item-name">
+                {item.isSelected ?(
+                  <>
+                    <FontAwesomeIcon icon={faCheckCircle} />
+                    <span className="completed">{item.itemName}</span>
+                  </>
+                ):(
+                  <>
+                    <FontAwesomeIcon icon={faCircle} />
+                    <span>{item.itemName}</span>
+                  </>
+                )}
+              </div>
+              <div className="quantity">
+                <button>
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+                <span>{item.quantity}</span>
+                <button>
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+              </div>
+            </div>
+          ))}
 				</div>
 				<div className="total">Total: </div>
 			</div>
